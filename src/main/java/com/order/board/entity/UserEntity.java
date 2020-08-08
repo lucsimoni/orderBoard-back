2ecf232f189efc26ca5b8ca70f11fae3e8e24979
lucsimoni <contact.lucsimoni@gmail.com>
2020-08-08 14:16:49 +0200

    modification entite user

--- src/main/java/com/order/board/entity/UserEntity.java
+++ src/main/java/com/order/board/entity/UserEntity.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.sun.istack.NotNull;
 
 @Entity
@@ -25,6 +27,24 @@ public class UserEntity {
 	@NotNull
 	@Column(name = "firstname")
 	private String firstname;
+
+	@NotNull
+	@Column(name = "role")
+	private String role;
+	
+	@NotNull
+	@Column(name = "login")
+	private String login;
+	
+	@NotNull
+	@Column(name = "password")
+	private String password;
+	
+	@NotNull
+	@DateTimeFormat
+	@Column(name = "lastconnection")
+	private String lastConnection;
+	
 	
 	public Long getId() {
 		return id;
@@ -49,5 +69,108 @@ public class UserEntity {
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+	
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getLastConnection() {
+		return lastConnection;
+	}
+
+	public void setLastConnection(String lastConnection) {
+		this.lastConnection = lastConnection;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "UserEntity [id=" + id + ", name=" + name + ", firstname=" + firstname + ", role=" + role + ", login="
+				+ login + ", password=" + password + ", lastConnection=" + lastConnection + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastConnection == null) ? 0 : lastConnection.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserEntity other = (UserEntity) obj;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastConnection == null) {
+			if (other.lastConnection != null)
+				return false;
+		} else if (!lastConnection.equals(other.lastConnection))
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+	
+	
 
 }
--- src/main/resources/dumpSQL.sql
+++ src/main/resources/dumpSQL.sql
@@ -1,5 +1,12 @@
 CREATE TABLE `orderboard`.`user` (
 	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
-	firstname VARCHAR(255) NOT NULL
+	firstname VARCHAR(255) NOT NULL,
+	role VARCHAR(255) NOT NULL,
+	login VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	lastconnection DATE NOT NULL
 );
+
+INSERT INTO `orderboard`.`user` (`id`, `name`, `firstname`, `role`, `login`, `password`, `lastconnection`) VALUES (1, 'simoni', 'luc', 'admin', 'lsimoni', 'lsimoni123!', '2020-05-01');
+INSERT INTO `orderboard`.`user` (`id`, `name`, `firstname`, `role`, `login`, `password`, `lastconnection`) VALUES (2, 'gardet', 'romain', 'user', 'rgardet', 'rgardet123!', '2020-06-01');
