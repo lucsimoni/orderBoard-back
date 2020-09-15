328deda33a283994a11310f21e9ef88f06d25c75
lucsimoni <contact.lucsimoni@gmail.com>
2020-09-15 17:40:50 +0200

    mise en place de l implementation

--- src/main/java/com/order/board/service/UserService.java
+++ src/main/java/com/order/board/service/UserService.java
@@ -2,39 +2,29 @@ package com.order.board.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
-
 import com.order.board.entity.UserEntity;
-import com.order.board.repository.UserRepository;
 import com.order.dto.UpdateUserDto;
 
-@Service
-public class UserService {
-	
-	@Autowired
-	@Qualifier("userRepository")
-	private UserRepository userRepository;
-	
-	public UserEntity getById(Long id) {
-		return this.userRepository.findById(id).get();
-	}
-	
-	public List<UserEntity> getAll() {
-		return this.userRepository.findAll();
-	}
-	
-//	@Override pour impl
-	public UserEntity updateUser(UpdateUserDto user) {
-		final UserEntity userToUpdate = this.userRepository.findById(user.getId()).get();
-		if(userToUpdate == null) {
-			return null;
-		} else {
-			userToUpdate.setFirstname(user.getFirstName());
-			userToUpdate.setName(user.getName());
-			return this.userRepository.save(userToUpdate);
-		}
-	}
+public interface UserService {
 
+	/**
+	 * Récupère un utilisateur à partir de son id
+	 * @param id d'un utilisateur
+	 * @return utilisateur
+	 */
+	public UserEntity getById(Long id);
+	
+	/**
+	 * Récupère la liste de tous les utilisateurs
+	 * @return Liste d'utilisateurs
+	 */
+	public List<UserEntity> getAll();
+	
+	/**
+	 * Mis à jour d'informations utilisateur
+	 * @param user - id name firstname
+	 * @return l'utilisateur mis à jour
+	 */
+	public UserEntity updateUser(UpdateUserDto user);
+	
 }
