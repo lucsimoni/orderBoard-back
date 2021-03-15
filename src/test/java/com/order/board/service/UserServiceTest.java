fb7eda78ede20de611048d509c4597750c9930c9
lucsimoni <contact.lucsimoni@gmail.com>
2021-03-15 11:50:06 +0100

    mock upd

--- src/test/java/com/order/board/service/UserServiceTest.java
+++ src/test/java/com/order/board/service/UserServiceTest.java
@@ -1,8 +1,14 @@
 package com.order.board.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.UUID;
 
 import com.order.board.BoardException;
+import com.order.board.service.implementation.UserServiceImpl;
+import com.order.dto.CreateUserDto;
+import com.order.dto.UpdateUserDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ServiceConfig.class)
@@ -16,6 +22,7 @@ public class UserServiceTest {
 	
 	private static final String LOGIN_USER_OK = "lbramard";
 	private static final String EMAIL_OK = "test@mail.fr";
+	private static final String USERID_OK = "fcd45s-ffdf345-fspl12me-ty54";
 	
 	@Autowired
 	UserServiceImpl userService;
@@ -93,8 +100,103 @@ public class UserServiceTest {
 		assertTrue(listUsers != null);
 	}
 	
+	/**
+	 * Test echec mis à jour d'un utilisateur
+	 */
+	@Test
+	public void updateUserKo() throws BoardException {
+		final UpdateUserDto updateUser = new UpdateUserDto();
+		updateUser.setId(UUID.randomUUID());
+		try {
+			userService.updateUser(updateUser);
+		} catch (final BoardException e) {
+			assertTrue(e.getMessage() == BoardException.getErrorUserUpdatedMandatoryNotFound());
+		}
+	}
+	
+	/**
+	 * Test succès mis à jour d'un utilisateur
+	 */
+	@Test
+	public void updateUserOk() throws BoardException {
+		final UpdateUserDto updateUser = new UpdateUserDto();
+		updateUser.setFirstName("toto");
+		updateUser.setName("tutu");
+		updateUser.setId(UUID.fromString(USERID_OK));
+		userService.updateUser(updateUser);
+		assertTrue(userRepository.findOneById(updateUser.getId()).getLastName == LASTNAME_OK);
+	}
 	
 	
+	/**
+	 * On test si un utilisateur existe déjà
+	 */
+	@Test
+	public void isAlreadyExistTest() throws BoardException {
+		//Utilisateur existe déjà
+		assertTrue(userService.isExists(ID_USER_BOARD));
+	}
+	
+	/**
+	 * Test récupération des produits d'un utilisateur OK
+	 */
+	@Test
+	public void findProductsByUserOk() throws BoardException {
+		
+		final int countProducts = userService.findProducts(USER_ID).size();
+		
+		//Ajoute un produit à l'utilisateur
+		//TODO
+		
+		final int countProdutsAfterInsert = userService.findProducts(USER_ID).size();
+		assertEquals(countProducts + 1, countProdutsAfterInsert);
+	}
+	
+	/**
+	 * Test récupération d'un produit d'un user KO
+	 */
+	@Test
+	public void findProductsByUserKo() throws BoardException {
+		try {
+			userService.findProducts(null);
+		} catch(final BoardException e) {
+			assertTrue(e.getMessage() == BoardException.getErrorProductIDNotFound());
+		}
+		try {
+			userService.findProducts("");
+		} catch(final BoardException e) {
+			assertTrue(e.getMessage() == BoardException.getErrorProductIDNotFound());
+		}
+	}
+	
+	
+	/**
+	 * Test creation user
+	 */
+	@Test
+	public void createUserOk() throws BoardException {
+		final int countUsers = userService.getAll().size();
+		
+		final CreateUserDto newUser = new CreateUserDto();
+		newUser.setFirstName('toto');
+		newUser.setName('neo');
+		newUser.setLogin('bilbo');
+		userService.createUser(newUser);
+		
+		final int countUsersAfterInsert = userService.getAll().size();
+		
+		assertEquals(countUsers + 1, countUsersAfterInsert);
+	}
+	
+	
+	
+	/**
+	 
+	  final List<CreateUserDto> listUser = new ArrayList<>();
+	  Mockito.when(userService.getUsersByPlace(Mockito.anyString())).thenReturn(listUser);
+	  
+	 *
+	 */
 	
 	
 	
