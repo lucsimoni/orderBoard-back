25d1a1fe78b5fc4cc46b39150059075924658571
lucsimoni <contact.lucsimoni@gmail.com>
2021-04-24 20:00:55 +0200

    envoie message apres une exception

--- src/main/java/com/order/board/BoardException.java
+++ src/main/java/com/order/board/BoardException.java
@@ -1,37 +1,36 @@
 package com.order.board;
 
-import org.primefaces.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class BoardException extends Exception {	
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BoardException extends RuntimeException {	
 	
 	private static final long serialVersionUID = 7221378573797080804L;
 	
-	private static final String SEPARATOR = ":";
+	private static final String SEPARATOR = "_";
 	
 	// ---------- Erreur Test
 	private static final String ERROR_TEST = "00" + SEPARATOR;
 	private static final String TEST_A = "01";
+		
+    public BoardException() {
+        super();
+    }
+    public BoardException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public BoardException(String message) {
+        super(message);
+    }
+    public BoardException(Throwable cause) {
+        super(cause);
+    }
 	
-	private final String errorCode;
-	
-	public BoardException() {
-		super();
-		this.errorCode = "";
-	}
-
-	public BoardException(final String code) {
-		super(code);
-		this.errorCode = code;
-	}
-	
-	// 00:01
+	// 00_01
 	public String getErrorTest() {
 		return ERROR_TEST + TEST_A;
 	}
+
 	
-	public String getBoardException() {
-		final JSONObject jsonException = new JSONObject();
-		jsonException.put("errorCode", this.errorCode);
-		return jsonException.toString();
-	}
 }
